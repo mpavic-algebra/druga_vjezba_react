c9d50ca59eb2fdcd6b2751d06125af744af750dc
Matej Pavic <matej@Matejs-MacBook-Pro.local>
2023-09-04 20:37:44 +0200

    Example of modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import random from 'randomstring-ng'
+import random from 'randomstring-ng';
+// import NeBasTakoGlavni, { Naslov, Naslov2 as MaliNaslovic } from './Naslovi'
+import * as bundled from './Naslovi'
 
 function App() {
 
   return (
     <>
       <h1>{random.generate()}</h1>
+      {/* <NeBasTakoGlavni />
+      <Naslov />
+      <MaliNaslovic /> */}
+      <bundled.default />
+      <bundled.Naslov />
+      <bundled.Naslov2 />
     </>
   )
 }
---
+++ src/Naslovi.jsx
@@ -0,0 +1,19 @@
+export const Naslov = () => {
+  return (
+    <h2>Naslov</h2>
+  )
+}
+
+export const Naslov2 = () => {
+    return (
+      <h2>Naslov 2</h2>
+    )
+}
+
+const Glavni = () => {
+    return (
+      <h2>Glavni naslov</h2>
+    )
+}
+
+export default Glavni
